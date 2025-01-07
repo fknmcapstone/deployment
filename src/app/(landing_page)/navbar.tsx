8e3a851c84a951bfec2fc2ec3d1ea821529a0ddd
AliZe198 <zazzyzizi@gmail.com>
2025-01-06 21:20:49 -0500

    Tkt-015: Modify nav bar dropdown menu

--- src/app/(landing_page)/navbar.tsx
+++ src/app/(landing_page)/navbar.tsx
@@ -10,6 +10,110 @@ function Separator() {
   return <div className={styles.separator} />;
 }
 
+const ResearchMenu = () => (
+  <div data-cy="nav_research_submenu" className={styles.navBarSubMenu}>
+    <div>
+      <div data-cy="nav_intake_visuals" className={styles.intakeVisualsNavItem}>
+        Intake Visuals
+        <div className={styles.intakeVisualsSubMenu}>
+          <Link
+            className={styles.marginBlock}
+            data-cy="nav_sfps_general_information"
+            href="/intake_visuals/SFPs_general_information/"
+          >
+            SFPs General Information
+          </Link>
+          <Link
+            className={styles.marginBlock}
+            data-cy="nav_sfps_food_breakdown"
+            href="/intake_visuals/SFPs_food_breakdown/"
+          >
+            SFPs Food Breakdown
+          </Link>
+        </div>
+      </div>
+      <Separator />
+      <div data-cy="nav_stakeholders" className={styles.intakeVisualsNavItem}>
+        Stakeholders Perception
+        <div className={styles.intakeVisualsSubMenu}>
+          <Link
+            className={styles.marginBlock}
+            data-cy="nav_parents_preferences"
+            href="/stakeholders_perception/parents_preferences/"
+          >
+            Parents' Preferences
+          </Link>
+          <Link
+            className={styles.marginBlock}
+            data-cy="nav_parents_opinions"
+            href="/stakeholders_perception/parents_opinions/"
+          >
+            Parents' Opinions
+          </Link>
+        </div>
+      </div>
+    </div>
+    <Separator />
+    <div>
+      <Link
+        className={styles.marginBlock}
+        data-cy="nav_measurement_tool_assessment"
+        href="/measurement_tool_assessment/"
+      >
+        Measurement Tool Assessment
+      </Link>
+      <Separator />
+      <Link
+        className={styles.marginBlock}
+        data-cy="nav_sfp_components"
+        href="/sfp_components/"
+      >
+        SFP Components
+      </Link>
+      <Separator />
+      <Link
+        className={styles.marginBlock}
+        data-cy="nav_national_characteristics"
+        href="/national_characteristics/"
+      >
+        National Characteristics of SFPs
+      </Link>
+    </div>
+  </div>
+);
+
+const OngoingStudiesMenu = () => (
+  <div data-cy="nav_ongoing_studies_submenu" className={styles.navBarSubMenu}>
+    <Link
+      className={styles.marginBlock}
+      data-cy="nav_impact_of_sfps"
+      href="/ongoing_studies/uoft_school_food_study/"
+    >
+      University of Toronto, School Food Study
+    </Link>
+  </div>
+);
+
+const PublicationsMenu = () => (
+  <div data-cy="nav_publications_submenu" className={styles.navBarSubMenu}>
+    <Link
+      className={styles.marginBlock}
+      data-cy="nav_manuscripts"
+      href="/manuscripts/"
+    >
+      Manuscripts
+    </Link>
+    <Separator />
+    <Link
+      className={styles.marginBlock}
+      data-cy="nav_presentations"
+      href="/presentations/"
+    >
+      Presentations
+    </Link>
+  </div>
+);
+
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => {
@@ -37,7 +141,6 @@ const Navbar = () => {
 
         <div className={styles.menuButtonContainer}>
           <div className={styles.menuButton} onClick={toggle}>
-            {/* Menu icon */}
             <svg
               xmlns="http://www.w3.org/2000/svg"
               width="40"
@@ -60,142 +163,27 @@ const Navbar = () => {
 
           <div data-cy="nav_research" className={styles.researchNavItem}>
             Research
-            <div
-              data-cy="nav_research_submenu"
-              className={styles.navBarSubMenu}
-            >
-              <div
-                data-cy="nav_intake_visuals"
-                className={styles.intakeVisualsNavItem}
-              >
-                Intake Visuals
-                <div
-                  data-cy="nav_intake_visuals_submenu"
-                  className={styles.intakeVisualsSubMenu}
-                >
-                  <div className={styles.triangleLeft} />
-                  <Link
-                    className={styles.marginBlock}
-                    data-cy="nav_sfps_general_information"
-                    href="/intake_visuals/SFPs_general_information/"
-                  >
-                    SFPs General Information
-                  </Link>
-                  <Separator />
-                  <Link
-                    className={styles.marginBlock}
-                    data-cy="nav_sfps_food_breakdown"
-                    href="/intake_visuals/SFPs_food_breakdown/"
-                  >
-                    SFPs Food Breakdown
-                  </Link>
-                  <Separator />
-                </div>
-              </div>
-              <Separator />
-              <div
-                data-cy="nav_intake_visuals"
-                className={styles.intakeVisualsNavItem}
-              >
-                Stakeholders Perception
-                <div
-                  data-cy="nav_intake_visuals_submenu"
-                  className={styles.intakeVisualsSubMenu}
-                >
-                  <div className={styles.triangleLeft} />
-                  <Link
-                    className={styles.marginBlock}
-                    data-cy="nav_parents_preferences"
-                    href="/stakeholders_perception/parents_preferences/"
-                  >
-                    Parents' Preferences
-                  </Link>
-                  <Separator />
-                  <Link
-                    className={styles.marginBlock}
-                    data-cy="nav_parents_opinions"
-                    href="/stakeholders_perception/parents_opinions/"
-                  >
-                    Parents' Opinions
-                  </Link>
-                  <Separator />
-                </div>
-              </div>
-              <Separator />
-              <Link
-                className={styles.marginBlock}
-                data-cy="nav_measurement_tool_assessment"
-                href="/measurement_tool_assessment/"
-              >
-                Measurement Tool Assessment
-              </Link>
-              <Separator />
-              <Link
-                className={styles.marginBlock}
-                data-cy="nav_sfp_components"
-                href="/sfp_components/"
-              >
-                SFP Components
-              </Link>
-              <Separator />
-              <Link
-                className={styles.marginBlock}
-                data-cy="nav_national_characteristics"
-                href="/national_characteristics/"
-              >
-                National Characteristics of SFPs
-              </Link>
-              <Separator />
-            </div>
+            <ResearchMenu />
           </div>
 
           <div data-cy="nav_ongoing_studies" className={styles.researchNavItem}>
             Ongoing Studies
-            <div
-              data-cy="nav_ongoing_studies_submenu"
-              className={styles.navBarSubMenu}
-            >
-              <Link
-                className={styles.marginBlock}
-                data-cy="nav_impact_of_sfps"
-                href="/ongoing_studies/uoft_school_food_study/"
-              >
-                University of Toronto, School Food Study
-              </Link>
-              <Separator />
-            </div>
+            <OngoingStudiesMenu />
           </div>
 
           <Link data-cy="nav_news" href="/news/">
             News
           </Link>
+
           <div data-cy="nav_publications" className={styles.researchNavItem}>
             Publications
-            <div
-              data-cy="nav_publications_submenu"
-              className={styles.navBarSubMenu}
-            >
-              <Link
-                className={styles.marginBlock}
-                data-cy="nav_manuscripts"
-                href="/manuscripts/"
-              >
-                Manuscripts
-              </Link>
-              <Separator />
-              <Link
-                className={styles.marginBlock}
-                data-cy="nav_presentations"
-                href="/presentations/"
-              >
-                Presentations
-              </Link>
-              <Separator />
-            </div>
+            <PublicationsMenu />
           </div>
+
           <Link data-cy="nav_about_fknm" href="/about_fknm/">
             <p>About FKNM</p>
           </Link>
+
           <Link data-cy="nav_contact" href="/about_fknm/contact/">
             <p>Contact</p>
           </Link>
